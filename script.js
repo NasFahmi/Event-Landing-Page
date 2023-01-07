fbed98e0cbcc00d75e71e5368a9e8eb0905a10f4
Osas997 <larasatyt83@gmail.com>
2023-01-07 20:12:10 +0700

    done

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-btn = document.querySelector('.button-theme')
-btn_slider = document.querySelector('.btn-slider')
-test = document.querySelector('ul')
-btn.addEventListener('click',function(){
-    btn.classList.toggle('active')
-    btn_slider.classList.toggle('active')
-    document.body.classList.toggle('dark')
-    
-})
+btn = document.querySelector(".button-theme");
+btn_slider = document.querySelector(".btn-slider");
+test = document.querySelector("ul");
+const tombol = document.querySelector(".toggle2");
+const navRespon = document.querySelector("nav ul");
+tombol.addEventListener("click", () => {
+  tombol.classList.toggle("tod");
+  navRespon.classList.toggle("slide");
+  document
+    .querySelector(".toggle2 span:first-child")
+    .classList.toggle("silang1");
+  document
+    .querySelector(".toggle2 span:last-child")
+    .classList.toggle("silang2");
+  document
+    .querySelector(".toggle2 span:nth-child(2)")
+    .classList.toggle("scale");
+});
 
+btn.addEventListener("click", function () {
+  btn.classList.toggle("active");
+  btn_slider.classList.toggle("active");
+  document.body.classList.toggle("dark");
+});
